32f56b511c3b584eb4ff1214d9cb1d70fd9ba22b
Misayo Suzuki <msuzuki07@mylangara.ca>
2025-10-11 18:40:44 -0700

    user profile page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDP88zVX_yPRwOKZl_xJxqjph2GFBNuk2o",
+  authDomain: "street-reads.firebaseapp.com",
+  projectId: "street-reads",
+  storageBucket: "street-reads.firebasestorage.app",
+  messagingSenderId: "228045832951",
+  appId: "1:228045832951:web:4b6d868e05a72ab08a89f2"
+};
+
+export default firebaseConfig;
---
+++ main.js
@@ -0,0 +1,52 @@
+
+import firebaseConfig from "./firebase.js";
+
+async function fetchUsers() {
+  const colRefUser = collection(db, "users");
+  const snapshotUsers = await getDoc(colRefUser);
+  return snapshotUsers.docs.map(doc => ({ id: doc.id, ...doc.data()
+  }));
+}
+
+async function fetchLibraries() {
+  const colRefLibrary = collection(db, "streetLibraries");
+  const snapshotLibraries = await getDoc(colRefLibrary);
+  return snapshotLibraries.docs.map(doc => ({ id: doc.id, ...doc.data()
+  }));
+}
+
+async function fetchAllData() {
+  try {
+    const [users, libraries] = await Promise.all([
+      fetchUsers(),
+      fetchLibraries()
+    ]);
+
+    console.log(users);
+    console.log(libraries);
+  } catch (error) {
+    console.error("error")
+  }
+}
+
+fetchAllData();
+
+const eye = document.querySelector(".fa-eye");
+const slashEye = document.querySelector(".fa-eye-slash");
+const password = document.querySelector("#password");
+
+slashEye.style.display = "none";
+const originalText = password.textContent;
+password.textContent = "*".repeat(originalText.length);
+
+eye.addEventListener("click", () => {
+   eye.style.display = "none";
+   slashEye.style.display = "inline";
+   password.textContent = originalText;
+});
+
+slashEye.addEventListener("click", () => {
+  eye.style.display = "inline";
+  slashEye.style.display = "none";
+  password.textContent = "*".repeat(originalText.length);
+});
