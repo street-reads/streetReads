645af3a3e89e44534cf2d469da489f47780e86e4
Mie Takahashi <mietakahashi1993@gmail.com>
2025-10-25 22:41:54 -0700

    add comment

--- js/homepage.js
+++ js/homepage.js
@@ -410,6 +410,7 @@ function clearMarkers() {
     markers = [];
 }
 
+// for security
 function escapeHtml(s = '') {
     return String(s).replace(/[&<>"']/g, function(c) {
         return ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' })[c];
