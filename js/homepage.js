87a0de1d41f3e2db76dbb12a7e9ff66a309b6d59
cj1607 <ucheokorojefferson@gmail.com>
2025-10-29 02:54:05 +0100

    fix: homepage fix

--- js/homepage.js
+++ js/homepage.js
@@ -336,7 +336,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     try {
-        map = tt.map({
+        const map = tt.map({
             key: apiKey,
             container: mapContainer,
             center: [-123.10904462328836, 49.22895825651896],
